79b51b960300d38c1157c2fa438674f029156e43
maek <maek@paranoici.org>
2017-01-27 10:51:39 +0000

    Add unrealarena game support to q3map2

--- tools/quake3/q3map2/q3map2.h
+++ tools/quake3/q3map2/q3map2.h
@@ -1960,6 +1960,8 @@ Q_EXTERN game_t games[]
 	,
 								#include "game_xonotic.h" /* most be after game_quake3.h as they share defines! */
 	,
+								#include "game_unrealarena.h" /* most be after game_quake3.h as they share defines! */
+	,
 								#include "game_tremulous.h" /*LinuxManMikeC: must be after game_quake3.h, depends on #define's set in it */
 	,
 								#include "game_unvanquished.h"
